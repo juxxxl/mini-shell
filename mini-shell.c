4cb26259d94f6d9dddfb6fb9adeb4b8cba1e7849
juxxxl <222992189+juxxxl@users.noreply.github.com>
2026-04-18 23:26:34 +0200

    add this and this

--- mini-shell.c
+++ mini-shell.c
@@ -1,26 +1,49 @@
 #include <stdio.h>
 
+typedef struct {
+  char *name;
+  int (*func)(int argc, char **argv) 
+} Command;
+
 int main() {
     char command[100];
+    char current_path[1024]
     while (1) {
         printf("mini-shell> ");
         fgets(command, sizeof(command), stdin);
-        // Remove newline character from the end of the command
+
+        // remove newline character from the end of the command
         command[strcspn(command, "\n")] = 0;
 
         if (strcmp(command, "exit") == 0) {
             break;
         }
+        
 
-        system(command);
+        for (int i = 0; commands[i].name != NULL; i++) {    // idk what i did in this blob :cry:
+
+          if (strcmp(commands[i].name, command) == 0 ) {
+
+            commands[i].func(argc, argv[])
+
+          }
+
+        }
+
+    
     }
     return 0;
 }
 
-int ls(int argc, char *argv[]) {
+Command commands[] = {
+  { "cd", my_cd},
+  {NULL, NULL}
+};
 
-  
-  
-  return 0;
+
+void my_cd(int argc, char **argv) {
+
 }
 
+
+// To do: tokenize user command input /  make the cd command (only command i need to actually implement, for the rest ill fork execv and use built in unix commands, )
