14c9e6e71b5bd9491619d8ac438bdb6a53b15fff
juxxxl <222992189+juxxxl@users.noreply.github.com>
2026-04-21 21:45:06 +0200

    added tokenization / command execution / cd implementation / customized shell input line look

--- mini-shell.c
+++ mini-shell.c
@@ -1,15 +1,63 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <stdlib.h>
 
-typedef struct {
-  char *name;
-  int (*func)(int argc, char **argv) 
-} Command;
+int tokenize_command(char *command, char **argv) {
+
+  int argc = 0; 
+
+  char *token = strtok(command, " ");
+ 
+  while (token != NULL) {
+
+    argv[argc] = token;
+
+    argc++;
+
+    token = strtok(NULL, " ");
+
+  }
+
+  argv[argc] = NULL;
+
+  return argc;
+
+}
+
+void exec_command(char **argv) {
+
+  pid_t pid = fork();
+
+  if (pid == 0) {
+
+    execvp(argv[0], argv);
+
+    perror("execvp failed miserably my guy ");
+
+    exit(1) ;
+
+  }
+
+  else if ( pid > 0) {
+
+    wait(NULL);
+
+  }
+} 
+
+
 
 int main() {
     char command[100];
-    char current_path[1024]
+    char current_path[1024];
+    char *user = getlogin();
+    char hostname[256];
+    gethostname(hostname, sizeof(hostname));
+    getcwd(current_path, sizeof(current_path));
     while (1) {
-        printf("mini-shell> ");
+        printf("[%s||%s > %s]", user, hostname, current_path);
         fgets(command, sizeof(command), stdin);
 
         // remove newline character from the end of the command
@@ -19,31 +67,24 @@ int main() {
             break;
         }
         
+        char *argv[64];
+        
+        int argc = tokenize_command(command,argv);
 
-        for (int i = 0; commands[i].name != NULL; i++) {    // idk what i did in this blob :cry:
-
-          if (strcmp(commands[i].name, command) == 0 ) {
-
-            commands[i].func(argc, argv[])
-
-          }
-
+        if (strcmp(argv[0], "cd") == 0) {
+          chdir(argv[1]);
+          getcwd(current_path, sizeof(current_path));
+        }
+        else {
+          exec_command(argv);
         }
 
+
+        
     
     }
     return 0;
 }
 
-Command commands[] = {
-  { "cd", my_cd},
-  {NULL, NULL}
-};
 
-
-void my_cd(int argc, char **argv) {
-
-}
-
-
-// To do: tokenize user command input /  make the cd command (only command i need to actually implement, for the rest ill fork execv and use built in unix commands, )
+// to do: pipe / autocompletion
